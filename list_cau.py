e67f75dbe65da4105b7f9ae3da01a2fb4521362a
heavy9922 <yefersoncasti@gmail.com>
2021-04-12 23:02:04 -0500

    list_comprehensions activity

--- list_cau.py
+++ list_cau.py
@@ -4,10 +4,11 @@ def run():
 #        for i in range(1, 101):
 #                if i % 3 !=  0:
 #                        squares.append(i**2)
-        print(squares)
+#        print(squares)
 
+        multi = [i  for i in range(1, 100000) if i % 4 == 0 and i % 6 == 0 and  i % 9 == 0]
 
-
+        print(multi)
 
 #       natural_number = [1,2,3,4,5,6,7,8,9]
 
