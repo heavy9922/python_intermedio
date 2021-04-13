493ed497cb6c6e45808c4b6c94862df05faa15f3
heavy9922 <yefersoncasti@gmail.com>
2021-04-12 22:30:51 -0500

    list comprehensions

---
+++ list_cau.py
@@ -0,0 +1,10 @@
+def run():
+        natural_number = [1,2,3,4,5,6,7,8,9]
+
+        for num in natural_number:
+                cua = num**2
+                print(cua)
+                
+
+if __name__ == '__main__':
+        run()
