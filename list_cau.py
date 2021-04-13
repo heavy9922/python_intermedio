b606d19579570b6e732ce0a512096b48e3b8cd89
heavy9922 <yefersoncasti@gmail.com>
2021-04-12 22:40:56 -0500

    list comprehensions 2

--- list_cau.py
+++ list_cau.py
@@ -1,9 +1,18 @@
 def run():
-        natural_number = [1,2,3,4,5,6,7,8,9]
+        squares = []
 
-        for num in natural_number:
-                cua = num**2
-                print(cua)
+        for i in range(1, 101):
+                if i % 3 !=  0:
+                        squares.append(i**2)
+        print(squares)
+
+
+
+#       natural_number = [1,2,3,4,5,6,7,8,9]
+
+#       for num in natural_number:
+#              cua = num**2
+#                print(cua)
                 
 
 if __name__ == '__main__':
